79e05ab9c2066d115b48911db6446649019d323c
诺 <yandex.s@aliyun.com>
2020-08-18 16:23:16 +0800

    Http请求,response 解析

--- week02/HTTP/client.js
+++ week02/HTTP/client.js
@@ -1,5 +1,72 @@
 const net = require("net")
+class ResponseParser {
+  constructor() { 
+    this.WAITING_STATUS_LINE = 0;
+    this.WAITING_STATUS_LINE_END = 1;
+    this.WAITING_HEADER_NAME = 2;
+    this.WAITING_HEADER_SPACE = 3;
+    this.WAITING_HEADER_VALUE = 4;
+    this.WAITING_HEADER_LINE_END = 5;
+    this.WAITING_HEADER_BLOCK_END = 6;
+    this.WAITING_BODY = 7;
 
+    this.current = this.WAITING_STATUS_LINE;
+    this.statusLine = "";
+    this.headers = {};
+    this.headerName = "";
+    this.headerValue = "";
+    this.bodyParser = null;
+  }
+  receive (string) {
+    for (let i = 0; i < string.length; i++) {
+      this.receiveChar(string.charAt(i));
+    }
+  }
+  receiveChar (char) {
+    if(this.current === this.WAITING_STATUS_LINE) {
+      if(char === '\r') {
+        this.current = this.WAITING_STATUS_LINE_END;
+      } else {
+        this.statusLine += char;
+      }
+    } else if(this.current === this.WAITING_STATUS_LINE_END) {
+      if(char === '\n') {
+        this.current = this.WAITING_HEADER_NAME;
+      }
+    } else if(this.current === this.WAITING_HEADER_NAME) {
+      if(char === ":") {
+        this.current = this.WAITING_HEADER_SPACE;
+      } else if(char === '\r'){
+        this.current = this.WAITING_HEADER_BLOCK_END;
+      } else {
+        this.headerName += char;
+      }
+    }else if (this.current === this.WAITING_HEADER_SPACE) {
+      if(char === ' '){
+        this.current = this.WAITING_HEADER_VALUE;
+      }
+    } else if(this.current === this.WAITING_HEADER_VALUE) {
+      if(char === '\r') {
+        this.current = this.WAITING_HEADER_LINE_END;
+        this.headers[this.headerName] = this.headerValue;
+        this.headerName = "";
+        this.headerValue = "";
+      } else {
+        this.headerValue += char;
+      }
+    }else if(this.current === this.WAITING_HEADER_LINE_END) {
+      if(char === '\n') {
+        this.current = this.WAITING_HEADER_NAME;
+      }
+    }else if(this.current === this.WAITING_HEADER_BLOCK_END) {
+      if(char === '\n') {
+        this.current = this.WAITING_BODY;
+      }
+    } else if(this.current === this.WAITING_BODY) {
+      console.log(char)
+    }
+  }
+};
 class Request {
   constructor(options) {
     this.method = options.method || "GET";
@@ -15,15 +82,43 @@ class Request {
     if (this.headers["Content-Type"] === "application/json") {
       this.bodyText = JSON.stringify(this.body);
     } else if (this.headers["Content-Type"] === "application/x-www-form-urlencoded") {
-      this.bodyText = Object.keys(this.body).map(key => `${key} = ${encodeURIComponent(this.body[key])}`).join('&');
+      this.bodyText = Object.keys(this.body).map(key => `${key}=${encodeURIComponent(this.body[key])}`).join('&');
     }
 
     this.headers["Content-Length"] = this.bodyText.length;
   }
-  send () {
+  send (connection) {
     return new Promise((resolve, rejects) => {
-      
-    })
+      const parser = new ResponseParser;
+      if (connection) {
+        connection.write(this.toString());
+      } else {
+        connection = net.createConnection({
+          host: this.host,
+          port: this.port
+        }, () => {
+          connection.write(this.toString());
+        })
+      }
+      connection.on('data', (data) => {
+        console.log(data.toString());
+        parser.receive(data.toString());
+        if (parser.isFinished) {
+          resolve(parser.response);
+          connection.end();
+        }
+      });
+      connection.on('error', (err) => {
+        rejects(err);
+        connection.end();
+      });
+    });
+  }
+  toString () {
+    return `${this.method} ${this.path} HTTP/1.1\r
+${Object.keys(this.headers).map(key => `${key}: ${this.headers[key]}`).join('\r\n')}\r
+\r
+${this.bodyText}`
   }
 }
 void async function () {
